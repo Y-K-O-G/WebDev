65e6576e1ac42da8df27cc6da7263e0a6ec6f938
Y-K-O-G <96924211+Y-K-O-G@users.noreply.github.com>
2023-02-21 00:57:08 -0300

    Create Tempo_Ate_90_Anos.js

---
+++ Exercícios_JS_em_Português/Tempo_Ate_90_Anos.js
@@ -0,0 +1,12 @@
+function tempoAteOs90(idade) {
+ 
+    var diasTotais = 365 * 90;
+    var semanasTotais = 52 * 90;
+    var mesesTotais = 12 * 90;
+    var diasRestantes = diasTotais - (idade * 365);
+    var semanasRestantes = semanasTotais - (idade * 52);
+    var mesesRestantes = mesesTotais - (idade * 12);
+    console.log("Você tem " + diasTotais + " dias, " + semanasTotais + " semanas, e " + mesesTotais + " meses restantes até os 90 anos.");
+}
+
+tempoAteOs90(prompt ("Qual é a tua idade?"));  
