fa52184a06c639a53ea1984e6d208d7f6c050ca1
Y-K-O-G <96924211+Y-K-O-G@users.noreply.github.com>
2023-02-21 01:29:22 -0300

    Create Exercise_1.js

---
+++ JS_Exercises_in_English/Exercise_1.js
@@ -0,0 +1,16 @@
+//This exercise consists of exchanging the values of variables "a" and "b" without changing the indicated code or entering numbers.
+
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+    var c = a;
+    a = b;
+    b = c;
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
