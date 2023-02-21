61c6355fb5706761d0e1eb0a42c9c4a00a38cabd
Y-K-O-G <96924211+Y-K-O-G@users.noreply.github.com>
2023-02-21 00:58:31 -0300

    Update Time_Until_90yo.js

--- JS_Exercises_in_English/Time_Until_90yo.js
+++ JS_Exercises_in_English/Time_Until_90yo.js
@@ -1,9 +1,8 @@
 //A function exercise that tells the user how many days, weeks and months he/she has until he/she completes 90 years old based on his/her age.
 
-function lifeInWeeks(age) {
-    
-/************Don't change the code above************/    
-  var totalDays = 365 * 90;
+function timeLeft(age) {
+     
+    var totalDays = 365 * 90;
     var totalWeeks = 52 * 90;
     var totalMonths = 12 * 90;
     var daysLeft = totalDays - (age * 365);
@@ -12,11 +11,4 @@ function lifeInWeeks(age) {
     console.log("You have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left until you complete 90 years old.");
 }
 
-lifeInWeeks(prompt ("How old are you?"));  
-    //Write your code here.
-    
-    
-    
-    
-    
-/*************Don't change the code below**********/
+timeLeft(prompt ("How old are you?"));  
