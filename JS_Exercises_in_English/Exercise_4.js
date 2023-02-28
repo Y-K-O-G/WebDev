89acc372342b1c9afbd92d0f30a295db9b6be2db
Y-K-O-G <96924211+Y-K-O-G@users.noreply.github.com>
2023-02-28 14:41:42 -0300

    Update Exercise_4.js

--- JS_Exercises_in_English/Exercise_4.js
+++ JS_Exercises_in_English/Exercise_4.js
@@ -8,7 +8,7 @@ function isLeap(year) {
     if(isNaN(parseInt(year)) == true) {
         alert("The year must be a number.");
         return isLeap(prompt("Input a year."));
-    } if(isNaN(parseInt(year)) == false) {
+    } else {
         if (year % 4 == 0) {
             if (year % 100 == 0) {
                 if (year % 400 == 0) {
