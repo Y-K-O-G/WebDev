4f892fa00bf12b77f3e37c5f95cdcba4e0505c0d
Y-K-O-G <96924211+Y-K-O-G@users.noreply.github.com>
2023-03-01 20:04:24 -0300

    Create Guest_List_Exercise.js

---
+++ JS_Exercises_in_English/Guest_List_Exercise.js
@@ -0,0 +1,15 @@
+//A exercise about arrays that checks if the user is on a predefined guest list.
+
+var guestList = ["Angela" , "Jack" , "Pam" , "James", "Lara" , "Jason"];
+
+var guestName = prompt("What is your name?");
+
+var checkName = guestName.slice(0, 1).toUpperCase() + guestName.slice(1, guestName.lenght).toLowerCase();
+
+alert(checkName);
+
+if(guestList.includes(checkName)){
+    alert("Welcome to the party!!!")
+} else {
+    alert("Sorry, but you were not invited.")
+}
