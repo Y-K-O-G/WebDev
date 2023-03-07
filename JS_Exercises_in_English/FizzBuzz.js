5e5bf31288319d685780b7df85c56bc5d79eed31
Y-K-O-G <96924211+Y-K-O-G@users.noreply.github.com>
2023-03-07 20:28:32 -0300

    Update FizzBuzz.js

--- JS_Exercises_in_English/FizzBuzz.js
+++ JS_Exercises_in_English/FizzBuzz.js
@@ -1,6 +1,6 @@
 /*That is the FizzBuzz Challenge.
 Based on a game that kids play in England, the code must
-create an empty array, them add numbers to it from 1 to 100,
+create an empty array, then add numbers to it from 1 to 100,
 but for multiples of 3 it must add "Fizz" instead of the number,
 for multiples of 5 it must add "Buzz, ando for multiples of
 both 3 and 5, it must add "FizzBuzz".*/
@@ -32,5 +32,4 @@ console.log(numbers);
 
 /*My code prints only the final result because i didn't want it
 to print 100 different results until the final one and didn't want
-the user to type "fizzBuzz()" everytime to see each result. This code
-also limits the count to 100, so it won't run infinitely.*/
+the user to type "fizzBuzz()" everytime to see each result.*/
