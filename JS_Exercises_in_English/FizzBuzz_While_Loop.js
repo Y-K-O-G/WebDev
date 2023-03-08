4a1fca02dd00133d8a949040a7a0d283a4930c9e
Y-K-O-G <96924211+Y-K-O-G@users.noreply.github.com>
2023-03-08 01:49:28 -0300

    Create FizzBuzz_While_Loop.js

---
+++ JS_Exercises_in_English/FizzBuzz_While_Loop.js
@@ -0,0 +1,23 @@
+//FizzBuzz challenge using "while" loop.
+
+var numbers = [];
+
+var n = 1; 
+
+function fizzBuzz() {
+    while (n < 101) {
+        if (n % 3 == 0 && n % 5 == 0){
+            numbers.push("FizzBuzz");   
+        } else if (n % 3 == 0) {
+            numbers.push("Fizz");
+        } else if (n % 5 == 0) {
+            numbers.push("Buzz");
+        } else {
+            numbers.push(n)
+        }
+        n++; 
+    }
+    console.log(numbers);
+}
+
+fizzBuzz();
