48b4ac832aeb131db30d8c08c89e3ffdff3195bf
Y-K-O-G <96924211+Y-K-O-G@users.noreply.github.com>
2023-03-08 01:27:36 -0300

    Create 99_Bottles.js

---
+++ Exercícios_JS_em_Português/99_Bottles.js
@@ -0,0 +1,25 @@
+/*Um código que escreve a música "99 Bottles of Beer" de "Melvins".*/
+
+var bottlesOfBeer = 99;
+
+function beer() {
+
+    while (bottlesOfBeer > 1) {
+        alert(bottlesOfBeer + " bottles of beer on the wall, " + bottlesOfBeer + " bottles of beer.\nTake one down and pass it around, " + (bottlesOfBeer - 1) + " bottles of beer on the wall.");
+        bottlesOfBeer--;
+    }
+    
+    if (bottlesOfBeer == 1) {
+        alert("1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, No more bottles of beer on the wall.");
+        bottlesOfBeer--;
+        beer();
+    }
+
+    if (bottlesOfBeer == 0) {
+        alert("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.");
+        bottlesOfBeer--;
+        beer();   
+    }
+}
+
+beer();
