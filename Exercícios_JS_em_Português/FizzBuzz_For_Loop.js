c79af9bcab27b75d02d8d86be1df4ef52514b7b8
Y-K-O-G <96924211+Y-K-O-G@users.noreply.github.com>
2023-03-08 01:47:07 -0300

    Create FizzBuzz_For_Loop.js

---
+++ Exercícios_JS_em_Português/FizzBuzz_For_Loop.js
@@ -0,0 +1,20 @@
+//Desafio FizzBuzz usando o loop "for".
+
+var numbers = [];
+
+function fizzBuzz() {
+    for (n = 1; n < 101; n++) {
+        if (n % 3 == 0 && n % 5 == 0){
+            numbers.push("FizzBuzz");   
+        } else if (n % 3 == 0) {
+            numbers.push("Fizz");
+        } else if (n % 5 == 0) {
+            numbers.push("Buzz");
+        } else {
+            numbers.push(n)
+        }
+    }
+    console.log(numbers);
+}
+
+fizzBuzz();
